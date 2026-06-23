ac3049ae63254417a841639272b1e3860849e7b2
vignesh <95468303+vigneshvvv@users.noreply.github.com>
2026-06-23 08:57:12 +0530

    Enhance StackandQueue with TreeSet and HashMap
    Added TreeSet and HashMap examples to demonstrate usage.

--- StackandQueue.java
+++ StackandQueue.java
@@ -1,9 +1,13 @@
 package sample;
 
 import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Stack;
+import java.util.TreeSet;
 
 public class StackandQueue {
 	public static void main(String[] args) {
@@ -30,9 +34,32 @@ public class StackandQueue {
 		System.out.println(integers.poll());
 		
 		
+		TreeSet<Integer> integers2 = new TreeSet<>();
+		integers2.add(20);
+		integers2.add(30);
+		integers2.add(10);
+		integers2.add(50);
+		integers2.add(5);
+		integers2.add(10);
+
+		
+		System.out.println(integers2);
+		
+		Map<Integer, String> studentDetails = new HashMap<>();
+		studentDetails.put(101, "Deva");
+		studentDetails.put(102, "Revanth");
+		studentDetails.put(103, "Subhash");
+		System.out.println(studentDetails.get(103));
 		
 		
+		for(Map.Entry<Integer, String> sample: studentDetails.entrySet()) {
+			System.out.println(sample.getKey());
+			System.out.println(sample.getValue());
+		}
 		
+		for(Integer key: studentDetails.keySet()) {
+			System.out.println("The Key is" + key);
+		}
 		
 		
 		
